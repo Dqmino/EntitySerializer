cdb93c68c6efc7e316a0fccae14004fecedfe9da
2Hex <rxgamerhere@gmail.com>
2022-03-06 15:02:00 +0100

    Features added

--- src/main/java/me/hex/entityserializer/EntitySerializer.java
+++ src/main/java/me/hex/entityserializer/EntitySerializer.java
@@ -5,18 +5,22 @@ import me.hex.entityserializer.core.StructureFactory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.structure.StructureManager;
 
-import java.util.Random;
-
 /**
  * API's Main class, holding the core and essentials of the library.
  */
 public final class EntitySerializer {
 
-    private JavaPlugin plugin;
-    private EntityHolder entityHolder;
-    private StructureFactory factory;
-    private Random random;
-    private StructureManager manager;
+    private final JavaPlugin plugin;
+    private final EntityHolder entityHolder;
+
+    /**
+     * @param plugin Main class of your plugin.
+     */
+    public EntitySerializer(JavaPlugin plugin) {
+        this.plugin = plugin;
+        StructureManager manager = plugin.getServer().getStructureManager();
+        this.entityHolder = new EntityHolder(manager, new StructureFactory(manager));
+    }
 
     /**
      * Gets the Main instance.
@@ -28,70 +32,12 @@ public final class EntitySerializer {
     }
 
     /**
-     * Initializes the API, With the User's Main Instance.
-     * REQUIRED TO USE THE API.
-     *
-     * @param plugin User's Main Instance.
-     */
-    public void setPlugin(JavaPlugin plugin) {
-        this.plugin = plugin;
-        initialize();
-    }
-
-    /**
-     * Checks if the main class instance was set.
-     *
-     * @return true if set, otherwise false.
-     */
-    public boolean isPluginSet() {
-        return plugin != null;
-    }
-
-    /**
-     * IMPORTANT NOTE: THIS SHOULD NOT BE USED OUTSIDE OF API INTERNALS. THIS IS NOT FOR YOU.
-     *
-     * @return StructureManager Object
-     */
-    public StructureManager getManager() {
-        return manager;
-    }
-
-    /**
      * you should use this to serialize, and to deserialize.
      *
      * @return EntityHolder Object.
      */
     public EntityHolder getEntityHolder() {
         return entityHolder;
-    }
-
-    /**
-     * Initializes variables.
-     * IMPORTANT NOTE: THIS SHOULD NOT BE USED OUTSIDE OF API INTERNALS. THIS IS NOT FOR YOU.
-     */
-    private void initialize() {
-        manager = plugin.getServer().getStructureManager();
-        factory = new StructureFactory(manager);
-        entityHolder = new EntityHolder(manager, factory);
-        random = new Random();
-    }
-
-    /**
-     * IMPORTANT NOTE: THIS SHOULD NOT BE USED OUTSIDE OF API INTERNALS. THIS IS NOT FOR YOU.
-     *
-     * @return StructureFactory Instance
-     */
-    public StructureFactory getFactory() {
-        return factory;
-    }
-
-    /**
-     * Gets the random object used for Structure API methods.
-     *
-     * @return Random Object.
-     */
-    public Random getRandom() {
-        return random;
     }
 
 }
